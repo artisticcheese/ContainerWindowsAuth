946c9acfadaff3f47f0de36f050388d9b2c56cc9
Suvalian, Greg <Greg_Suvalian@McAfee.com>
2017-09-06 13:19:59 -0500

    Initial

---
+++ Plain/Models/Index.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Plain.Models
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; private set; } = "In page model: ";
+
+        public void OnGet()
+        {
+            Message += User.Identity.Name;
+        }
+    }
+}
